263488522ebc176ebdc4c21af47bdc0927b043b1
FanZhong Zeng <fanzhongzeng78@gmail.com>
2019-10-24 12:09:04 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,20 +3,11 @@ def nyc_pigeon_organizer(data)
   newData = {}
   data.each do |key, value|
     #Key = color, gender, lives
+    puts "key: #{key}, value: #{value}"
     if key = :color
-      value.each do |attrOfKey, name|
-        if newData[name]
-          if newData[name][key]
-            #newData[name][key].add(attrOfKey)
-          else
-            newData[name][key] = [attrOfKey]
-          end
-        else
-          newData[name] = name
-        end
-      end
+     
     end
   end
   
-  puts "New Data #{newData}"
+  #puts "New Data #{newData}"
 end
